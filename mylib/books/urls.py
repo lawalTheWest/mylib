e7cdf59859d18ef9ff3617bf6425029b61d56d2b
lawal tajudeen ogunsola <lwltjdn@gmail.com>
2025-06-02 17:50:13 +0100

    added a download feature for each book

--- mylib/books/urls.py
+++ mylib/books/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.book_list, name='book_list'),
     path('book/<uuid:pk>/', views.book_detail, name='book_detail'),
+    path('download/<uuid:pk>/', views.download_book, name='download_book'),
 ]
--- mylib/books/views.py
+++ mylib/books/views.py
@@ -1,3 +1,4 @@
+from django.http import FileResponse, Http404
 from django.shortcuts import render, get_object_or_404
 from .models import Book
 
@@ -9,4 +10,10 @@ def book_list(request):
 def book_detail(request, pk):
     book = get_object_or_404(Book, pk=pk)
     return render(request, 'books/book_detail.html', {'book': book})
-    
+
+def download_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if not book.file:
+        raise Http404("File not found.")
+    response = FileResponse(book.file.open('rb'), as_attachment=True, filename=book.file.name.split('/')[-1])
+    return response
